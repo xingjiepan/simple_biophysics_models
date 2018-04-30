62d720991559a98ac81cef37b70d1129de3c478d
xingjiepan <xingjiepan@gmail.com>
2018-04-30 16:30:45 -0700

    Write some reaction rates to the script reaction.py

---
+++ statistical_mechanics/reaction.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+reaction_rates = [
+        {'reaction':'atp_hydrolysis', 'buffer':'water', 't_half':'116 hours'},
+        {'reaction':'atp_hydrolysis', 'buffer':'Mg2+_pH1.52', 't_half':'42 min'},
+        {'reaction':'atp_hydrolysis', 'buffer':'Mg2+_pH6.59', 't_half':'27.8 hours'},
+        {'reaction':'atp_hydrolysis', 'buffer':'Ca2+_pH1.40', 't_half':'40 min'},
+        {'reaction':'atp_hydrolysis', 'buffer':'Ca2+_pH7.01', 't_half':'5.8 hours'},
+        
+        {'reaction':'DNA_hydrolysis', 'buffer':'water', 't_half':'30,000,000 years'},
+        
+        {'reaction':'peptide_bond_hydrolysis', 'buffer':'water', 't_half':'7 years'},
+        ]
