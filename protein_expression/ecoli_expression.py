a40191ac42890d8c52143c289016f9a074134a53
xingjiepan <xingjiepan@gmail.com>
2018-08-31 16:15:36 -0700

    Update the ecoli_expression.py script

--- protein_expression/ecoli_expression.py
+++ protein_expression/ecoli_expression.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
+ecoli_dry_weight_composition = {
+        'protein' : 0.55,
+        'RNA' : 0.2, # Mainly from ribosome
+        'DNA' : 0.03,
+        'lipid' : 0.09,
+        'lipopolysaccharide' : 0.03,
+        'peptidoglycan' : 0.03,
+        'metabolites' : 0.03,
+        'inorganic_ions' : 0.01,
+        }
 
 def OD600_to_wet_ecoli_cell_weight(od600):
     '''Convert OD600 to cell density in 
@@ -9,7 +19,9 @@ def OD600_to_wet_ecoli_cell_weight(od600):
 
 def ecoli_cell_wet_weight_to_dry_weight(wet_weight):
     '''Convert ecoli cell wet weight to dry weight.'''
-    return 0.2 * wet_weight
+    return 0.3 * wet_weight
 
 if __name__ == '__main__':
-    pass
+    
+    print('OD600=1 ecoli have protein {0} g/L'.format(ecoli_dry_weight_composition['protein'] * ecoli_cell_wet_weight_to_dry_weight(
+        OD600_to_wet_ecoli_cell_weight(1))))
